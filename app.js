4879bdc01e37f20657a50388a8d06cc08010c6d6
kaisergrimlock <s3891987@rmit.edu.vn>
2022-05-31 19:33:14 +0700

    Update app.js
    create server

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const fs = require('fs');
-fs.writeFileSync("hello.txt", "Hello World");
+const http = require('http');
+
+const server = http.createServer(function rqListener(req, res){
+    console.log(req)
+});
+
+server.listen(3000);
