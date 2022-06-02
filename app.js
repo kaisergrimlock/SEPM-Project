7af01980348ac142e9fa8663fd8f9d0f3a0d8551
kaisergrimlock <s3891987@rmit.edu.vn>
2022-06-02 17:02:30 +0700

    install express js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const http = require('http');
 
+const express = require('express');
+
+const app = express();
+
 const server = http.createServer(function rqListener(req, res){
     console.log(req)
 });
