e3ed9b7f2ad5b3ad500e35e6d8219b85055c4171
kaisergrimlock <s3891987@rmit.edu.vn>
2022-06-14 13:38:36 +0700

    update register

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+//Import express and body-parser
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//Import bcrypt to encrypt password
+const bcrypt = require('bcrypt')
+
 const app = express();
 
+const users = []
+
+//Declare view-engine
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(express.urlencoded({extended: false}))
 
+//Routers
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
@@ -14,7 +23,27 @@ app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+          id: Date.now().toString(),
+          name: req.body.name,
+          email: req.body.email,
+          password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(users)
+})
+
 app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', (req, res, next) => {
     res.render('login.ejs')
 })
 
