8846c6995add04922d2a4d752609421d22f2dbba
kaisergrimlock <s3891987@rmit.edu.vn>
2022-06-16 11:01:18 +0700

    save session functionalities
    Save session to mongodb for authentication and access

--- app.js
+++ app.js
@@ -13,10 +13,19 @@ const bcrypt = require('bcrypt')//for encryption of password
 const mongoose = require("mongoose")
 const flash = require('express-flash')//error display
 const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 
 //Express views
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+
+//Session middleware
+const MONGODB_URI = 'mongodb+srv://Khoi:1234@cluster0.owhumte.mongodb.net/user'
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+})
+app.use(session({secret: 'SEPM', resave: false, saveUninitialized: false, store: store}))
 
 //Controllers & Routers
 app.get('/', (req, res) => {
@@ -41,14 +50,14 @@ app.post('/login', (req, res) => {
       if(doMatch) {
         //Return to homepage
         console.log('Login success')
-        return res.redirect('/')
-        // req.session.isLoggedIn = true
-        // req.session.user = user
-        // return req.session.save(err => {
-        //   console.log(err)
-        //   console.log('Redirected')
-        //   res.redirect('/')
-        // })
+        req.session.isLoggedIn = true
+        req.session.user = user
+        return req.session.save(err => {
+          console.log(err)
+          console.log('Redirected')
+          res.redirect('/')
+        })
+        // return res.redirect('/')
       }else{
         console.log('Wrong password')
         return res.redirect('/login')
