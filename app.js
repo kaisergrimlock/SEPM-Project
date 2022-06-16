43821b4ca05c309655de3dee500a8e373f8360c9
kaisergrimlock <s3891987@rmit.edu.vn>
2022-06-16 10:18:33 +0700

    login function
    Recreate login function, temporarily remove passport.js and its functionalities.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+//Import user model
+User = require('./models/user')
+
 //Import 3rd party libraries
 const express = require('express')//import express
 const app = express()
@@ -13,66 +16,59 @@ const passportLocalMongoose = require('passport-local-mongoose')
 const flash = require('express-flash')//error display
 const session = require('express-session')
 const methodOverride = require('method-override')
-const initializePassport = require('./passport-config')
-initializePassport(
-  passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
-)
-
-//Import user model
-const users = []
-User = require('./models/user')
-
-//Mongoose setters
-// mongoose.set('useNewUrlParser', true);
-// mongoose.set('useFindAndModify', false);
-// mongoose.set('useCreateIndex', true);
-// mongoose.set('useUnifiedTopology', true);
-// mongoose.connect("mongodb://localhost/auth_demo_app");
 
 //Express views
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
-//Login
-app.use(flash())
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
-
 //Controllers & Routers
-app.get('/', checkAuthenticated, (req, res) => {
+app.get('/', (req, res) => {
   res.render('index.ejs', { name: req.user.name })
 })
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
+app.get('/login', (req, res) => {
   res.render('login.ejs')
 })
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}))
+app.post('/login', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  User.findOne({email: email}).then(user => {
+    if(!user) {
+      console.log('No such user')
+      return res.redirect('/login')
+    }
+    bcrypt.compare(password, user.password).then(doMatch => {
+      if(doMatch) {
+        console.log('Login success')
+        req.session.isLoggedIn = true
+        req.session.user = user
+        return req.session.save(err => {
+          console.log(err)
+          res.redirect('/')
+        })
+      }else{
+        console.log('Wrong password')
+        return res.redirect('/login')
+      }
+    }).catch(err => {
+      return res.redirect('/login')
+    })
+  })
+})
 
-app.get('/register', checkNotAuthenticated, (req, res) => {
+app.get('/register', (req, res) => {
   res.render('register.ejs')
 })
 
-app.post('/register', checkNotAuthenticated, async (req, res) => {
+app.post('/register', async (req, res) => {
   const name = req.body.name
   const email = req.body.email
-  const password = req.body.password
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
   const user = new User({
     name: name,
     email: email,
-    password: password
+    password: hashedPassword
   })
   user
   .save()
@@ -84,40 +80,7 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
   .catch(err => {
     console.log(err);
   })
-  // try {
-  //   const hashedPassword = await bcrypt.hash(req.body.password, 10)
-  //   users.push({
-  //     id: Date.now().toString(),
-  //     name: req.body.name,
-  //     email: req.body.email,
-  //     password: hashedPassword
-  //   })
-  //   res.redirect('/login')
-  // } catch {
-  //   res.redirect('/register')
-  // }
 })
-
-//Logout
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
-})
-
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-
-  res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/')
-  }
-  next()
-}
 
 //Connect to Mongoose
 mongoose.connect('mongodb+srv://Khoi:1234@cluster0.owhumte.mongodb.net/user?retryWrites=true&w=majority').then(result => {
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-
 const Schema = mongoose.Schema
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
     name: {
@@ -17,4 +17,5 @@ const userSchema = new Schema({
     }
 })
 
+userSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model('User', userSchema)
