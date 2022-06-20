76d63c5e66b1662bfa002d9749d53ee59b7a4037
kaisergrimlock <s3891987@rmit.edu.vn>
2022-06-20 10:28:17 +0700

    Bug fix mongodb
    Fixed mongodb bug where there can't be more than one users.

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
     name: {
@@ -17,5 +16,4 @@ const userSchema = new Schema({
     }
 })
 
-userSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model('User', userSchema)
