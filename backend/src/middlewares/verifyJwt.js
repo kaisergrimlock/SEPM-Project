56deeb1647da1a4e8771198321a43d4df2b4e35e
Minh Nguyens <wormscott12397@gmail.com>
2022-07-11 13:31:53 +0700

    use catchAsync in middlewares verifyJwt to structurally catch err

--- backend/src/middlewares/verifyJwt.js
+++ backend/src/middlewares/verifyJwt.js
@@ -1,16 +1,15 @@
 const Error = require('../config/constant/Error');
 const { ResponseService, JwtService } = require('../services');
+const { catchAsync } = require('../utils');
 
-module.exports = async (req, res, next) => {
+module.exports = catchAsync(async (req, res, next) => {
   const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
   const authHeader = req.headers.authorization;
-  if (!authHeader) next(ResponseService.newError(Error.AuthHeaderMissing.errCode, Error.AuthHeaderMissing.errMessage));
+  if (!authHeader) throw ResponseService.newError(Error.AuthHeaderMissing.errCode, Error.AuthHeaderMissing.errMessage);
   const token = authHeader.split(' ')[1];
   if (!token) throw ResponseService.newError(Error.JwtMissing.errCode, Error.JwtMissing.errMessage);
-  try {
-    await JwtService.authenticateJWT(token, accessTokenSecret);
-  } catch (err) {
-    next(err);
-  }
+
+  await JwtService.authenticateJWT(token, accessTokenSecret);
+
   next();
-};
+});
