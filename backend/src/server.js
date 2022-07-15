d1478b50516a7ccd0634c2014f9653033e9e05b4
Minh Nguyens <wormscott12397@gmail.com>
2022-07-15 23:03:41 +0700

    this one deploy work

--- backend/src/server.js
+++ backend/src/server.js
@@ -106,12 +106,11 @@ const server = app.listen(port, () => {
 
 // Deployment
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../frontend/build')));
+  app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
+    res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
     res.end();
-    console.log('inside serving');
   });
 }
 
