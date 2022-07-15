24c3c1f012b671e84378f04ac5bbfaf14ffe4909
Minh Nguyens <wormscott12397@gmail.com>
2022-07-15 22:56:32 +0700

    hope fix bug deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -109,8 +109,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/build'), 'index.html');
+    res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
     res.end();
+    console.log('inside serving');
   });
 }
 
