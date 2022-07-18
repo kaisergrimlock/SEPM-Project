cae7f6227b1915c0282abbfe6303c09de40d64df
Bui Quang An <diablo@Buis-MacBook-Pro.local>
2022-07-18 21:36:20 +0700

    restructure fe

---
+++ frontend/src/views/RegisterPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {RegisterForm} from '../components/register/RegisterForm'
+export const RegisterPage = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center sm:px-0 px-5'>
+        <RegisterForm/>
+    </div>
+  )
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.js', './public/index.html'
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    extend: {
+      colors:{
+        darkBlue: "#0E465D",
+        darkBlue2: "#10526D",
+        cyan: "#39B7ED",
+        lightBlue: "#EBF6FB",
+        lightGreen: "#E9F8F6",
+        lightGreen2: "#EEFFFC",
+        lightRed: "#F6ECE9",
+        lightOrange: "#FEEFEB",
+        lightPurple: "#F2F3FF",
+        yellow: "#DDB40A"
+      },
+      fontFamily: {
+        avenir: ["Avenir", "sans-serif"],
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  
+  plugins: [],
 }
