447a20bae2d44e8ddc4bf0cbdd7a2e38a3abd91c
kaisergrimlock <s3891987@rmit.edu.vn>
2022-07-26 21:02:40 +0700

    socket.io update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./views/HomePage";
 import {LoginPage} from "./views/LoginPage"
 import { RegisterPage } from "./views/RegisterPage";
+import io from 'socket.io-client';
+const io = io.connect('http://localhost:3000');
+
 function App() {
   return (
     <div>
